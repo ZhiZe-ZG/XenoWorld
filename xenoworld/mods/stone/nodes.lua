03b222b88f7bdd249172da33ddb021b85149295f
Qi Wang <qi_wang_zz@outlook.com>
2025-08-06 14:41:11 +0800

    fix stone texture and register node

---
+++ xenoworld/mods/stone/nodes.lua
@@ -0,0 +1,8 @@
+minetest.register_node("stone:stone", {
+	description = S("Stone"),
+	tiles = {"stone.png"},
+	groups = {cracky = 3, stone = 1},
+	drop = "stone:stone",
+	legacy_mineral = true,
+	sounds = default.node_sound_stone_defaults(),
+})
