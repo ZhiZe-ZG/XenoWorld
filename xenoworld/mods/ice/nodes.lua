d650747462c80904e56408fbfeb510893c4d4f9e
Qi Wang <qi_wang_zz@outlook.com>
2025-08-06 16:08:06 +0800

    Fix ice translation

--- xenoworld/mods/ice/nodes.lua
+++ xenoworld/mods/ice/nodes.lua
@@ -1,5 +1,8 @@
+-- Load support for MT game translation.
+local S = minetest.get_translator(minetest.get_current_modname())
+
 minetest.register_node("ice:ice", {
-	description = "Ice",-- S("Stone"),
+	description = S("Ice"),
 	tiles = {"ice.png"},
 	groups = {dig_immediate=2},
 	drop = "ice:ice",
