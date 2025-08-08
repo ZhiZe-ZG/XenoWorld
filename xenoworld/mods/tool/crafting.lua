99090ae8a70f6935e08b15d8f110389f25ea5d20
Qi Wang <qi_wang_zz@outlook.com>
2025-08-08 13:04:25 +0800

    fix recipe

--- xenoworld/mods/tool/crafting.lua
+++ xenoworld/mods/tool/crafting.lua
@@ -2,7 +2,7 @@ minetest.register_craft({
 	output = "tool:pick_stone",
 	recipe = {
 		{"xenoworld_rock:limestone_piece", "xenoworld_rock:limestone_piece", "xenoworld_rock:limestone_piece"},
-        {"", "xenoworld_wood:piece_of_wood", ""},
-		{"", "xenoworld_wood:piece_of_wood", ""},
+        {"", "xenoworld_wood:wood_piece", ""},
+		{"", "xenoworld_wood:wood_piece", ""},
 	}
 })
